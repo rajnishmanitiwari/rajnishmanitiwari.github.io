062009568d45dda00cd4c3c8b8a8e61d2da9a1fc
Rajnish Mani Tiwari <20bcs3950@cuchd.in>
2022-11-02 19:18:13 +0530

    no idea php contact

--- forms/contact.php
+++ forms/contact.php
@@ -1,18 +1,45 @@
-<?php
-     $name = $_POST['name'];
-     $email = $_POST['email'];
-     $message = $_POST['message'];
-
-    $email_from = 'Rajnish Mani Tiwari';
-    $email_subject = 'New Message From Rajnish Contact'
-    $email_body =  "Name: $name.\n".
-                   "Email: $email.\n".
-                   "Message: $message.\n";
-                   
-     $to ="mr.rajnishmani@gmail.com";
-     $headers = "From: $email_from \r\n";
-     $headers .= "Reply-To: $email \r\n";
-    
-     mail($to,$email_subject,$email_body,$headers);
-    header ("location: index.html");
+<?php  
+if( isset($_POST['submit']) ) {
+//getting user data
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$fromEmail = $_POST['email'];
+$phone = $_POST['tel'];
+$dateOfBirth = $_POST['date'];
+$cityName = $_POST['city'];
+$homeAddress = $_POST['address'];
+$employmentStatus = $_POST['status'];
+ 
+//Recipient email, Replace with your email Address
+$mailTo = 'mr.rajnishmani@gmail.com';
+ 
+//email subject
+$subject = ' A New Message Received From ' .$firstName;
+ 
+//email message body
+$htmlContent = '<h2> Email Request Received </h2>
+<p> <b>Client Name: </b> '.$firstName . " " . $lastName . '</p>
+<p> <b>Email: </b> '.$fromEmail .'</p>
+<p> <b>Phone Number: </b> '.$phone .'</p>
+<p> <b>Date of Birth: </b> '.$dateOfBirth .'</p>
+<p> <b>City Name: </b> '.$cityName .'</p>
+<p> <b>Home Address: </b> '.$homeAddress .'</p>
+<p> <b>Employment Status: </b> '.$employmentStatus .'</p>';
+ 
+//header for sender info
+$headers = "From: " .$firstName . "<". $fromEmail . ">";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+ 
+//PHP mailer function
+ $result = mail($mailTo, $subject, $htmlContent, $headers);
+ 
+   //error checking
+   if($result) {
+    $success = "The message was sent successfully!";
+   } else {
+    $failed = "Error: Message was not sent, Try again Later";
+   }
+}
+ 
 ?>
